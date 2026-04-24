43212b122a0c0199bf657fdf47c5a0eba6d95bf9
Alexandr Shishov <ernesto.chrome@mail.ru>
2026-04-24 13:43:18 +0300

    feat: variables;

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var str string
+	var integer int
+	var boolean bool
+	var floatNumber float64
+
+	fmt.Println(str, "- str", integer, "- integer", boolean, "- boolean", floatNumber, "- floatNumber")
 }
